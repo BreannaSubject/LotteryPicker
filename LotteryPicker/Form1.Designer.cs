aaea9121f20277ff796fc0a6d595e1d541315de0
Breanna Subject <subjectkids@gmail.com>
2020-05-05 09:33:13 -0400

    Add project files.

---
+++ LotteryPicker/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LotteryPicker
+{
+    partial class LotteryNumberPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.numberButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkCyan;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.titleLabel.Location = new System.Drawing.Point(-2, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(645, 70);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lottery Number Generator";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionsLabel.Location = new System.Drawing.Point(66, 88);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(509, 20);
+            this.instructionsLabel.TabIndex = 1;
+            this.instructionsLabel.Text = "Press the button below to generate your winning 6/49 numbers";
+            // 
+            // numberButton
+            // 
+            this.numberButton.BackColor = System.Drawing.Color.DarkCyan;
+            this.numberButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.numberButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.numberButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.numberButton.Location = new System.Drawing.Point(272, 125);
+            this.numberButton.Name = "numberButton";
+            this.numberButton.Size = new System.Drawing.Size(87, 34);
+            this.numberButton.TabIndex = 2;
+            this.numberButton.Text = "Generate";
+            this.numberButton.UseVisualStyleBackColor = false;
+            this.numberButton.Click += new System.EventHandler(this.numberButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(67, 186);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(495, 106);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // LotteryNumberPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(641, 339);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.numberButton);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "LotteryNumberPicker";
+            this.Text = "Lottery Number Picker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Button numberButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ LotteryPicker/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryPicker
+{ 
+    public partial class LotteryNumberPicker : Form
+    {
+        Random randGen = new Random();
+        int numValue1,numValue2,numValue3,numValue4,numValue5,numValue6;
+        const int MAX = 50;
+        public LotteryNumberPicker()
+        {
+            numValue1 = randGen.Next(1, MAX);
+            numValue2 = randGen.Next(1, MAX);
+            numValue3 = randGen.Next(1, MAX);
+            numValue4 = randGen.Next(1, MAX);
+            numValue5 = randGen.Next(1, MAX);
+            numValue6 = randGen.Next(1, MAX);
+            InitializeComponent();
+        }
+
+        private void numberButton_Click(object sender, EventArgs e)
+        {
+
+            while (numValue1 == numValue2) 
+            {
+                numValue2 = randGen.Next(1, MAX);
+            }
+
+            while (numValue3 == numValue1)
+            {
+                numValue3 = randGen.Next(1, MAX);
+            }
+
+            while (numValue4 == numValue3)
+            {
+                numValue4 = randGen.Next(1, MAX);
+            }
+
+            while (numValue5 == numValue2)
+            {
+                numValue5 = randGen.Next(1, MAX);
+            }
+
+            while (numValue6 == numValue1)
+            {
+                numValue6 = randGen.Next(1, MAX);
+            }
+
+            outputLabel.Text = Convert.ToString(numValue1) + " " + Convert.ToString (numValue2) + " " + Convert.ToString(numValue3) + " " ;
+            outputLabel.Text += Convert.ToString(numValue4) + " " + Convert.ToString (numValue5) + " " + Convert.ToString(numValue6); 
+        }
+    }
+}
